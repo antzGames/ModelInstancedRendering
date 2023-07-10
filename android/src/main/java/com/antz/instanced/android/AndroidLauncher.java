cfbc529b24fbba0c86d4acaae34c8e18128a259f
Anthony Iantria <anthony.iantria@gmail.com>
2023-07-10 16:06:52 -0400

    Initial commit

---
+++ android/src/main/java/com/antz/instanced/android/AndroidLauncher.java
@@ -0,0 +1,19 @@
+package com.antz.instanced.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.antz.instanced.ModelInstancedRendering;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        configuration.useGL30 = true;
+        initialize(new ModelInstancedRendering(), configuration);
+    }
+}
